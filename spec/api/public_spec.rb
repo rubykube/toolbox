7264d1efa47887a0ca69b82b007f9c0a945f4905
andreymakovenko <makovenkoandrey@gmail.com>
2018-12-19 17:09:44 +0200

    Add public/markets endpoint to specs

--- spec/api/public_spec.rb
+++ spec/api/public_spec.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+require 'spec_helper'
 require 'time'
 
 RSpec.describe 'Peatio public api' do
@@ -13,8 +14,19 @@ RSpec.describe 'Peatio public api' do
   describe 'get /public/timestamp' do
     let(:request) { public_host.get('timestamp') }
 
-    it 'Should respond with a timestamp' do
+    it 'should respond with a timestamp' do
       expect(Time.parse(request.body)).to be_a_kind_of(Time)
+    end
+  end
+
+  # TODO: rewrite to match real markets data
+
+  describe 'get /public/markets' do
+    let(:request) { public_host.get('markets') }
+
+    it 'should respond with array of hashes' do
+      expect(request.body).to be_an_instance_of(Array)
+      expect(request.body.first).to be_an_instance_of(Hash)
     end
   end
 end
