7bfd2d5d96d913f96b12f63932a11b58fcea31b3
Yaroslav Savchuk <savchukyarpolk@gmail.com>
2019-02-25 17:34:30 +0200

    Make auditor configurable

--- lib/toolbox/auditors/base.rb
+++ lib/toolbox/auditors/base.rb
@@ -1,7 +1,16 @@
 module Toolbox::Auditors
   class Base
-    def initialize
 
+    # TODO: Custom config file.
+    def initialize
+      binding.pry
+      @config = Config.load_and_set_settings(default_conf_file)
+      puts @config
+      # @config = File.exists?()
+    end
+
+    def default_conf_file
+      File.join(TOOLBOX_ROOT, "config", "#{self.class.name.demodulize.downcase}.yml")
     end
 
     def run!
