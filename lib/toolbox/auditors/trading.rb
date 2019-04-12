3aafe7be5cbba4438b0662972ef9fb7b7e1a4e62
Valentine Shatravenko <vshatravenko@heliostech.fr>
2019-04-12 17:31:05 +0300

    Finalize the display fixes

--- lib/toolbox/auditors/trading.rb
+++ lib/toolbox/auditors/trading.rb
@@ -39,7 +39,7 @@ module Toolbox::Auditors
     #   12 of 200 orders created (0.69 seconds passed).
     # TODO: Order creation failures processing.
     def create_and_run_workers
-      Kernel.print "Creating #{@order.number} orders... "
+      Kernel.print "Creating #{@orders.number} orders... "
       Array.new(@threads_number) do
         Thread.new do
           loop do
@@ -62,7 +62,7 @@ module Toolbox::Auditors
       File.open(report_file, 'w') do |f|
         f.puts YAML.dump(compute_report)
       end
-      puts "Report output to #{report_file_path}"
+      puts "Report saved to #{report_file}"
     end
 
     def compute_report
